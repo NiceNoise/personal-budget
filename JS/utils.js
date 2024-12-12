6e8ed86a4ed58b737be483d8b5a83ce66ac5c93d
NiceNoise <gsegura@undiadesun.com>
2024-12-12 02:29:55 -0500

    feat: avance 1 mejora interfaz

--- JS/utils.js
+++ JS/utils.js
@@ -48,3 +48,70 @@ function semaforoDolares(monto){
           break;;
       }
 }
+
+
+
+//----------------------------------------------------------------
+//---------------- Inicio: Category Dinámico ---------------------
+//----------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  //variables DOM
+  const typeTransaction = document.querySelectorAll('input[name="type"]');
+  const selectCategory = document.getElementById('category');
+
+  // Categorías definidas
+  const categories = {
+      credit: [
+          { value: 'salary', text: 'Salary' },
+          { value: 'investments', text: 'Investments' },
+          { value: 'freelance', text: 'Freelance' },
+          { value: 'others', text: 'Others' }
+      ],
+      debit: [
+          { value: 'feeding', text: 'Feeding' },
+          { value: 'transport', text: 'Transport' },
+          { value: 'services', text: 'Services' },
+          { value: 'rent', text: 'Rent' },
+          { value: 'outfit', text: 'Outfit' },
+          { value: 'studies', text: 'Studies' },
+          { value: 'entertainment', text: 'Entertainment' },
+          { value: 'others', text: 'Others' }
+      ]
+  };
+
+  // Función para actualizar las categorías
+  function updateCategory(type) {
+      // Limpiar select actual
+      selectCategory.innerHTML = '';
+
+      // Cargar categorías según el tipo de movimiento
+      categories[type].forEach(category => {
+          const option = document.createElement('option');
+          option.value = category.value;
+          option.textContent = category.text;
+          selectCategory.appendChild(option);
+      });
+  }
+
+  // Agregar evento a los radio buttons
+  typeTransaction.forEach(radio => {
+      radio.addEventListener('change', (e) => {
+          if (e.target.checked) {
+            updateCategory(e.target.value);
+          }
+      });
+  });
+
+  // Inicializar con categorías de ingreso
+  const selectedRadio = document.querySelector('input[name="type"]:checked');
+  if (selectedRadio) {
+      updateCategory(selectedRadio.value);
+  }
+
+});
+
+//----------------------------------------------------------------
+//---------------- Fin: Category Dinámico ---------------------
+//----------------------------------------------------------------
